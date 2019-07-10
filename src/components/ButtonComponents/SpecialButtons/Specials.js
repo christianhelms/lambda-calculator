06e78ae7bacf143a44c36e6791f8f20c0a494278
christianhelms <fonticons-dev@Fonticonss-MBP.hitronhub.home>
2019-07-10 17:15:50 -0500

    mvp

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import SpecialButton from "./SpecialButton";
 
 //import any components needed
 
 //Import your array data to from the provided data file
+import { specials } from "../../../data";
 
-const Specials = () => {
+const Specials = (props) => {
   // STEP 2 - add the imported data to state
+  const [special, setSpecial] = useState(specials);
 
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {special.map(spec => (
+        <SpecialButton spec={spec} update={props.update} />
+      ))}
     </div>
   );
 };
+
+export default Specials;
--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,6 @@
 import React from "react";
 
-const Display = () => {
-  return <div>{/* Display any props data here */}</div>;
+const Display = (props) => {
+  return <div>{props.calc}</div>;
 };
+export default Display
